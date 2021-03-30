7496a07f71e0b33f7296e61df9ef1c5d188bd782
newbie-newb <userA@my_email.com>
2021-03-30 22:17:23 +0900

    뮤직 서버

--- musictest/src/main/java/com/cos/myjpa/service/SongService.java
+++ musictest/src/main/java/com/cos/myjpa/service/SongService.java
@@ -4,6 +4,7 @@ import java.util.List;
 
 import org.springframework.stereotype.Service;
 
+import com.cos.myjpa.domain.song.CategoryType;
 import com.cos.myjpa.domain.song.Song;
 import com.cos.myjpa.domain.song.SongRepository;
 
@@ -27,6 +28,18 @@ public class SongService {
 		List<Song> songList = songRepository.findAll();
 		return songList;
 	}
+	
+	public List<Song> 카테고리리스트(String category){ //나중에 키워드로 get요청해서 안드로이드 어댑터와 동기화
+		List<Song> songList = songRepository.mfindCategory(category);
+		return songList;
+	}
+	
+	public List<Song> 검색리스트(String keyword){
+		List<Song> searchSongList = songRepository.mfindByKeyword(keyword);
+		
+		return searchSongList;
+	}
+	
 	
 	
 }
